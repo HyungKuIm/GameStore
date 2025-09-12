25b13a7fffb18be2aa6a9e059b74f3b305f332f7
HyungKu Im <damansa1@naver.com>
2025-09-12 18:17:05 +0900

    컴포넌트에 파라미터 사용

--- GameStore.FrontEnd/Clients/GameClient.cs
+++ GameStore.FrontEnd/Clients/GameClient.cs
@@ -49,4 +49,22 @@ public class GameClient
 
         games.Add(GameSummary);
     }
+
+    public GameDetails GetGame(int id)
+    {
+        GameSummary? game = games.Find(game => game.Id == id);
+        ArgumentNullException.ThrowIfNull(game);
+
+        var genre = genres.Single(genre => string.Equals(genre.Name, game.Genre,
+            StringComparison.OrdinalIgnoreCase));
+
+        return new GameDetails
+        {
+            Id = game.Id,
+            Name = game.Name,
+            GenreId = genre.Id.ToString(),
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
 }
